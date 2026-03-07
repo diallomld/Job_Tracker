64a01bd01e21c871e52f6263cc8ffe73059598f5
Lamine <diallomld5@gmail.com>
2026-03-07 10:34:14 +0100

    fix eslint errors

--- src/__tests__/hooks/useLocalStorage.test.js
+++ src/__tests__/hooks/useLocalStorage.test.js
@@ -1,5 +1,6 @@
 import { renderHook, act } from '@testing-library/react';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 
 describe('useLocalStorage', () => {
     beforeEach(() => {
